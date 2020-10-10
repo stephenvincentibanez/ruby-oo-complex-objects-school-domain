dd9e1faa4e4ec29de23fb716d188b5c3155be162
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-10 11:15:42 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+
+attr_accessor :school, :roster, :student_name, :grade
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def roster 
+        @roster
+    end
+
+    def add_student(name, grade)
+        @student_name = name 
+        @grade = grade
+        if @roster.include?(grade) == false
+        roster[grade] = []
+        end 
+        roster[grade] << name
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort
+        end 
+    end
+
+end
+
+# pomperaug = School.new("Pomperaug High School")
